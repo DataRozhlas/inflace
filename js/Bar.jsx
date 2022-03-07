e43cfd5f11dcb197204e7760b99b51d7e2903939
Petr Kočí <petr.koci@samizdat.cz>
2022-03-07 10:00:40 +0100

    rámečky kolem barů

--- js/Bar.jsx
+++ js/Bar.jsx
@@ -5,13 +5,16 @@ function Bar({ n, v, v2 }) {
   return (
     <div className="bar">
       <Typography variant={"caption"}>{n}</Typography>
-      <svg height={"1em"} width={"100%"}>
+      <svg height={"1.1em"} width={"100%"}>
         <rect height={"1em"} width={`${v2 / 10}%`} fill="#7aaacc"></rect>
         <rect
-          height={"1em"}
           width={`${v / 10}%`}
+          x={0.5}
+          y={0.5}
+          height={"1em"}
           fill="none"
           stroke="black"
+          strokeWidth="1px"
         ></rect>
       </svg>
     </div>
--- js/Buttons.jsx
+++ js/Buttons.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { Button, ButtonGroup } from "@mui/material";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 
 function Buttons({ isMobile }) {
   return (
     <div className={"components buttons"}>
-      <ButtonGroup
-        orientation="vertical"
+      <ToggleButtonGroup
+        exclusive
+        color="primary"
+        orientation={isMobile ? "vertical" : "horizontal"}
         fullWidth={isMobile}
-        sx={{ minWidth: isMobile ? "100%" : "50%" }}
       >
-        <Button>Domácnosti celkem</Button>
-        <Button>Domácnosti důchodců</Button>
-        <Button>Domácnosti v Praze</Button>
-      </ButtonGroup>
-      <ButtonGroup
-        orientation="vertical"
-        fullWidth={isMobile}
-        sx={{ minWidth: isMobile ? "100%" : "50%" }}
-      >
-        <Button>Skupina 4</Button>
-        <Button>Skupina 5</Button>
-        <Button>Skupina 6</Button>
-      </ButtonGroup>
+        <ToggleButton selected>Domácnosti celkem</ToggleButton>
+        <ToggleButton>Domácnosti důchodců</ToggleButton>
+        <ToggleButton>Domácnosti v Praze</ToggleButton>
+
+        <ToggleButton>Domácnosti důchodců</ToggleButton>
+        <ToggleButton>Domácnosti důchodců</ToggleButton>
+        <ToggleButton>Domácnosti důchodců</ToggleButton>
+      </ToggleButtonGroup>
     </div>
   );
 }
