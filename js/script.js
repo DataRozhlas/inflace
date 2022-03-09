37bee4133e49664e5cfd2d07cf36b49d4248af8e
datastory <honza@datastory.cz>
2022-03-09 20:59:59 +0100

    analytika horizontalniho scrollu

--- js/script.js
+++ js/script.js
@@ -11,6 +11,25 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 ReactDOM.render(<Switcher />, document.getElementById("switcher"));
 
+// analytics zda se scrolluje doprava
+function anlSend(jsn) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://x27m1i3il0.execute-api.eu-central-1.amazonaws.com/prod', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(jsn));
+};
+
+const uid = Date.now().toString() + Math.floor(Math.random() * 100000000000000).toString();
+anlSend({'uid': uid, 'e': 'load'})
+const horScr = document.getElementsByClassName('scrollx');
+let fired = 0;
+horScr[0].addEventListener('scroll', (e) => {
+  if (fired === 0) {
+    fired = 1;
+    anlSend({'uid': uid, 'e': 'xscrl'})
+  }
+})
+
 /*
 // snadné načtení souboru pro každého!
 fetch("https://blabla.cz/blabla.json")
